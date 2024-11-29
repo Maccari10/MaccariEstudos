50f3fb1611469db38620a8f9f781678c9d56a8a4
Maccari10 <00001106078858sp@al.educacao.sp.gov.br>
2024-11-29 08:07:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Matemática',
+    'O que é arranjo?',
+    'É um conceito da matemática que se refere a uma seleção de objetos em que a ordem dos elementos é importante'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'Qual a principal característica de um biomarcador?',
+    'Um indicador biológico que mede processos normais, psicológicos ou respostas a intervenções terapêuticas'
+)
+
+criaCartao(
+    'História',
+    'Qual foi a principal causa da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando, herdeiro do trono austro-húngaro.'
+)
+
+criaCartao(
+    'Química',
+    'Qual a diferença entre um átomo e uma molécula?',
+    'Um átomo é a menor partícula de um elemento químico que mantém suas propriedades, enquanto uma molécula é formada por dois ou mais átomos ligados entre si. '
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual o principal objeto de estudo da Sociologia?',
+    'A sociedade humana. A Sociologia se dedica a estudar as relações sociais, as culturas e os processos sociais que moldam a vida em sociedade'
+)
+
+criaCartao(
+    'Português',
+    'O que é um sujeito na frase?',
+    'É o termo que indica quem ou o que realiza a ação ou sofre o estado descrito pelo verbo.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são paralelos e meridianos?',
+    'Linhas imaginárias usadas para determinar a localização na Terra. Paralelos medem latitude e meridianos medem longitude.'
+)
+
+criaCartao(
+    'História',
+    'O que foi a Revolução Industrial?',
+    'Foi um período de grandes mudanças nas formas de produção, marcado pela mecanização e uso de máquinas.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a principal função do sistema respiratório?',
+    'Trocar gases(absorver oxigênio e eliminar dióxido de carbono).'
+)
